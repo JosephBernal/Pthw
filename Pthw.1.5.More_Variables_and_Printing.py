5b4f9da279eefd527fafb375e458f892164dc260
axecopfire <braun.sn11@gmail.com>
2017-09-13 15:48:23 -0400

    1.5 conv in->cm/lb->kg

--- Pthw.1.5.More_Variables_and_Printing.py
+++ Pthw.1.5.More_Variables_and_Printing.py
@@ -1,10 +1,19 @@
 name = 'Zed A. Shaw'
 age = 35 # not a lie
-height = 74 # inches
-weight = 180 # lbs
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
+
+# height
+height = 74 # inches
+cm_per_inch = 2.54
+height_cm = height * cm_per_inch # conversion cm --> in
+
+# weight
+weight = 180 # lbs
+kg_per_lb = 0.453592
+weight_kg = weight * kg_per_lb # conversion lbs --> kg
+
 
 print(f"Let's talk about {name}.")
 print(f"He's {height} inches tall.")
@@ -16,3 +25,4 @@ print(f"His teeth are usually {teeth} depending on the coffee.")
 # this liine is tricky, try to get it exactly right
 total = age + height + weight
 print(f"If I add {age}, {height}, and {weight} I get {total}.")
+print(f"His height in centimeters is {height_cm} and his weight in kilograms is {weight_kg}.")
