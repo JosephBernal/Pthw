d1f75606535ab82afb2d75785d974c56a78f91de
axecopfire <axecopfire@users.noreply.github.com>
2017-09-13 20:55:34 -0400

    1.6 Comments added

--- Pthw.1.6.Strings_and_Text.py
+++ Pthw.1.6.Strings_and_Text.py
@@ -1,21 +1,31 @@
+# Var assignment of int
 types_of_people = 10
+# Var assignment to formatted string
 x = f"There are {types_of_people} types of people."
 
+# Var assignment to string literals
 binary = "binary"
 do_not = "don't"
+
+# Var assignment to more formatted string
 y = f"Those who know {binary} and those who {do_not}."
 
+# Print formatted strings
 print(x)
 print(y)
 
+# Print formatted var strings
 print(f"I said: {x}")
 print(f"I also said: '{y}'")
 
+# Joke with variable assigned to Bool
 hilarious = False
 joke_evaluation = "Isn't that joke so funny?! {}"
 
+# Joke printed
 print(joke_evaluation.format(hilarious))
 
+# Concatenated strings
 w = "This is the left side of..."
 e = "a string with a right side."
 print(w + e)
