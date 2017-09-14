64a66e3d8a011cd6eaa1f991149e96e6ebd1987c
axecopfire <axecopfire@users.noreply.github.com>
2017-09-13 21:11:15 -0400

    1.6 Bin quantified

--- Pthw.1.6.Strings_and_Text.py
+++ Pthw.1.6.Strings_and_Text.py
@@ -3,6 +3,9 @@ types_of_people = 10
 # Var assignment to formatted string
 x = f"There are {types_of_people} types of people."
 
+# The actual binary joke
+act_num_of_peep = 4
+
 # Var assignment to string literals
 binary = "binary"
 do_not = "don't"
@@ -10,6 +13,7 @@ do_not = "don't"
 # Var assignment to more formatted string
 y = f"Those who know {binary} and those who {do_not}."
 
+z = f"{act_num_of_peep} is the actual number of people who know how to use binary"
 # Print formatted strings
 print(x)
 print(y)
@@ -17,6 +21,7 @@ print(y)
 # Print formatted var strings
 print(f"I said: {x}")
 print(f"I also said: '{y}'")
+print(f"I think this is the actual number: {z}")
 
 # Joke with variable assigned to Bool
 hilarious = False
