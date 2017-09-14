2a0c98a0e2f6e1e877d59c3461dae42075195f45
axecopfire <braun.sn11@gmail.com>
2017-09-14 15:45:54 -0400

    Pthw.1.10 Base

--- Pthw.1.10.What_Was_That.py
+++ Pthw.1.10.What_Was_That.py
@@ -4,9 +4,9 @@ backslash_cat = "I'm \\ a \\ cat."
 
 fat_cat = """
 I'll do a list:
-\t* Cat food
-\t* Fishies
-\t* Catnip\n\t* Grass
+\r* Cat food
+\r* Fishies
+\r* Catnip\n\t* Grass
 """
 print(tabby_cat)
 print(persian_cat)
