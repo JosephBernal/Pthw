3183b8718b189b2ed335836375fff5953c46d573
axecopfire <axecopfire@users.noreply.github.com>
2017-09-18 20:15:17 -0400

    Ex17 Tried to put all text on one line however text editor auto corrected

--- Exercises/Ex17_More_Files/More_Files.py
+++ Exercises/Ex17_More_Files/More_Files.py
@@ -1,22 +1,33 @@
-from sys import argv
-from os.path import exists
+# from sys import argv
+# from os.path import exists
 
-script, from_file, to_file = argv
+#script, from_file, to_file = argv
 
 # print(f"Copying from {from_file} to {to_file}")
 
 # we could do these two on one line, how?
-in_file = open(from_file)
-indata = in_file.read()
+# in_file = open(from_file)
+# indata = in_file.read()
 
-print(f"The input file is {len(indata)} bytes long")
+
+# print(f"The input file is {len(indata)} bytes long")
 # print("Ready, hit RETURN to continue, CTRL-C to abort.")
 # input()
 
+# out_file = open(to_file, 'w')
+# out_file.write(indata)
+
+#print("Alright, all done.")
+
+# out_file.close()
+# in_file.close()
+
+from sys import argv
+from os.path import exists
+script, from_file, to_file = argv
+in_file = open(from_file)
+indata = in_file.read()
 out_file = open(to_file, 'w')
 out_file.write(indata)
-
-print("Alright, all done.")
-
 out_file.close()
 in_file.close()
