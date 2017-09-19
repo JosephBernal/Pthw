075af8efcefab429939c6d4d737e9c15a4d24eef
axecopfire <axecopfire@users.noreply.github.com>
2017-09-19 19:39:03 -0400

    Ex19 Text edited

--- Exercises/Ex19_Functions_and_Variables.py
+++ Exercises/Ex19_Functions_and_Variables.py
@@ -2,6 +2,7 @@ def cheese_and_crackers(cheese_count, boxes_of_crackers):
     print(f"You have {cheese_count} cheeses!")
     print(f"You have {boxes_of_crackers} boxes of crackers!")
     print("Man that's enough for a party!")
+    print("Get a blanket.\n")
 
 
 print("We can just give the function numbers directly:")
@@ -17,4 +18,4 @@ print("We can even do math inside too:")
 cheese_and_crackers(10 + 20, 5 + 6)
 
 print("And we can combine the two, variables and math:")
-cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000
+cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
