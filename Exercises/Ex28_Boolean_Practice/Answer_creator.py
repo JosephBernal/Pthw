8c2cffb61de4bd43372f36664d2623f88a0d3ca7
axecopfire <axecopfire@users.noreply.github.com>
2017-09-28 10:14:25 -0400

    Answer-Creator

--- Exercises/Ex28_Boolean_Practice/Answer_creator.py
+++ Exercises/Ex28_Boolean_Practice/Answer_creator.py
@@ -5,13 +5,19 @@ script, in_file, out_file = argv
 in_txt = open(in_file)
 out_txt = open(out_file, "r+")
 
-def main(in_file, out_file,):
+def main(in_txt, out_txt,):
     line = in_txt.readline()
+
+    if line:
+        print_line(line)
+        return main(in_txt, out_txt)
  
 
-def print_line(in_file, out_file):
+def print_line(in_txt, out_txt):
     next_line = line.strip()
-    print(f'print("{line}")')
+    
+    print(f'print("{next_line}")')
+
 
 
 
