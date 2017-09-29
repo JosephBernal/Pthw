3f712e7423ff04f01c7fe7d56142e582a56a9cfb
axecopfire <axecopfire@users.noreply.github.com>
2017-09-28 22:52:19 -0400

    Ex28 Answer_sheet attempted. Sudo did not work

--- Exercises/Ex28_Boolean_Practice/Answer_creator.py
+++ Exercises/Ex28_Boolean_Practice/Answer_creator.py
@@ -5,21 +5,21 @@ script, in_file, out_file = argv
 in_txt = open(in_file)
 out_txt = open(out_file, "r+")
 
-def main(in_txt, out_txt,):
+
+def main(in_txt, out_txt):
     line = in_txt.readline()
 
     if line:
-        print_line(line)
+        print_line(line, out_txt)
         return main(in_txt, out_txt)
- 
 
-def print_line(in_txt, out_txt):
+def print_line(line, out_txt):
     next_line = line.strip()
-
-    print(f'print("{next_line}: "{next_line})')
-
+    out_txt.write(f'print("{next_line}: "{next_line}) \n')
 
 
+
+main(in_txt, out_txt)
 
 in_txt.close()
 out_txt.close()
