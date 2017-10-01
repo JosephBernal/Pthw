43c8bef81ff83fcb3f1616870ac1c8f15def896c
axecopfire <axecopfire@users.noreply.github.com>
2017-09-30 21:33:57 -0400

    Ex33 Base

--- Exercises/Ex32_Loops_and_Lists.py
+++ Exercises/Ex32_Loops_and_Lists.py
@@ -16,13 +16,15 @@ for i in change:
     print(f"I got {i}")
 
 # we can also build lists, first start with an empty one
-elements = []
+elements = list(range(0, 6))
+print(elements)
 
 # then use the range function to do 0 to 5 counts
-for i in range(0, 6):
-    print(f"Adding {i} to the list.")
-    # append is a function that lists understand
-    elements.append(i)
+# for i in range(0, 6):
+#print(f"Adding {i} to the list.")
+# append is a function that lists understand
+# elements.append(i)
+
 
 # now we can print them out too
 for i in elements:
