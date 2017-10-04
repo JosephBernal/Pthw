6557f532ac3d9605ef10caf6500dfb716bcee4c6
axecopfire <braun.sn11@gmail.com>
2017-10-04 13:09:40 -0400

    Ex36 Base

--- Exercises/Ex35_Branches_and_Functions.py
+++ Exercises/Ex35_Branches_and_Functions.py
@@ -3,17 +3,17 @@ from sys import exit
 def gold_room():
     print("This room is full of gold. How much do you take?")
 
-choice = input("> ")
-if "0" in choice or "1" in choice:
-    how_much = int(choice)
-else:
-    dead("Man, learn to type a number.")
+    choice = input('> ')
+    if "0" in choice or "1" in choice:
+        how_much = int(choice)
+    else:
+        dead("Man, learn to type a number.")
 
-if how_much < 50:
-    print("Nice, you're not greedy, you win!")
-    exit(0)
-else:
-    dead("You greedy bastard!")
+    if how_much < 50:
+        print("Nice, you're not greedy, you win!")
+        exit(0)
+    else:
+        dead("You greedy bastard!")
 
 
 def bear_room():
@@ -34,6 +34,7 @@ def bear_room():
             bear_moved = True
         elif choice == "taunt bear" and bear_moved:
             dead("The bear gets pissed off and chews your leg off.")
+        elif choice == "open door" and bear_moved:
             gold_room()
         else:
             print("I got no idea what that means.")
@@ -72,3 +73,6 @@ def start():
         cthulhu_room()
     else:
         dead("You stumble around the room until you starve.")
+
+
+start()
