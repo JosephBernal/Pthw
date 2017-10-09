37d8d4b08936b4cad5a038c992704ae71f27fe1f
axecopfire <braun.sn11@gmail.com>
2017-10-09 17:39:27 -0400

    Review and symbols added to ex36

--- Exercises/Ex36_Designing_and_Debugging.py
+++ Exercises/Ex36_Designing_and_Debugging.py
@@ -1,50 +1,38 @@
 from sys import exit
 
-def troll_riddle():
-    choice = input("> ")
-    if "me" or "author" or "coder" in choice:
-        print("You solved my riddle.")
-        print("You are very smart you know.")
-        prince_room()
-    else:
-        print("Get out of my sight!")
-        first_room()
-
-def troll_room():
-        print("There is a troll in front of you who is crouched in a sinister fashion.")
-        print("What do you do?")
-
-        choice = input("> ")
-
-        if "look" in choice:
-            print("It looks like a Japanese garden indoors.")
-            print("There is a bridge in the center of the room and a waterfall in the corner.")
-            print("The aesthetic is quite peaceful if it weren't for the ugly troll.")
-            troll_room()
-        elif "ask" or "question" in choice:
-            print("A riddle it is!")
-            print("Who has two thumbs and hates riddles?")
-            troll_riddle()
-        else:
-            print("The troll disappears and you walk around aimlessly for awhile...")
-            print("Finally getting bored you return to the last room.")
-            first_room()
-
-
-
-def prince_room():
-    print("Yay you got to the prince!")
-    print("He is forever grateful.")
-    print("He kills his father and becomes king.")
-    print("You have ruined the lands.")
-    print("Kudos.")
-
-
 def start():
     print("You were sent here to return something of value to the king.")
     print("This house is dark and full of terrors.")
     print("What do you do?")
     first_room()
+
+
+def first_room():
+    print("What do you do?")
+
+    choice = input("> ")
+
+    if "look" in choice:
+        print("You see a taxidermied bear. He looks mean.")
+        print("There is a life-size hourglass")
+        print("and there are two doors.")
+        first_room()
+
+    elif ("walk" or "open") and "door" and "1" in choice:
+        print("The door squeaks open.")
+        fight_room()
+
+    elif ("walk" or "open" or "door") and "2" in choice:
+        print("The door swings open with much aplumb.")
+        troll_room()
+
+    elif choice is "3":
+        raise AssertionError("You crazy.") 
+
+    else:
+        print("What're you on about?")
+        first_room()
+
 
 def fight_room():
 
@@ -68,25 +56,57 @@ def fight_room():
         you can't stand his complaining and just leave""")
         first_room()
 
-def first_room():
+def troll_room():
+    print("There is a troll in front of you who is crouched in a sinister fashion.")
+    print("What do you do?")
+
     choice = input("> ")
 
     if "look" in choice:
-        print("You see a taxidermied bear. He looks mean.")
-        print("There is a life-size hourglass")
-        print("and there are two doors.")
-        print("What do you do?")
-        first_room()
-    elif ("walk" or "open") and "door" and "1" in choice:
-        print("The door squeaks open.")
-        fight_room()
-
-    elif ("walk" or "open" or "door") and "2" in choice:
-        print("The door swings open with much aplumb.")
+        print("It looks like a Japanese garden indoors.")
+        print("There is a bridge in the center of the room and a waterfall in the corner.")
+        print("The aesthetic is quite peaceful if it weren't for the ugly troll.")
         troll_room()
-
+    elif "ask" or "question" in choice:
+        print("A riddle it is!")
+        print("Who has two thumbs and hates riddles?")
+        troll_riddle()
     else:
-        print("What are you on about?")
+        print("The troll disappears and you walk around aimlessly for awhile...")
+        print("Finally getting bored you return to the last room.")
         first_room()
+
+
+def troll_riddle():
+    choice = input("> ")
+    if "me" or "author" or "coder" in choice:
+        print("You solved my riddle.")
+        print("You are very smart you know.")
+        prince_room()
+    else:
+        print("Get out of my sight!")
+        first_room()
+
+
+def prince_room():
+
+    print("""Yay you got to the prince!
+    He is forever grateful.
+    He would like to present you with a token
+    of his appreciation.
+    Do you accept?""")
+    
+    choice = input("y/n")
+
+    while choice != "y":
+        choice = "y"
+        print("why you no like me?")
+        print("I like you.")
+        break
+
+    print("The prince kisses you on the cheek.")
+
+
+
 
 start()
---
+++ Refs/Symbol_Review.md
@@ -0,0 +1,85 @@
+# Keywords
+
+| Keywords | Description | Example|
+|- |- |- |
+| `and` | Logical and. | ` True and False == False` |
+| `as` | Part of the `with-as` statement. | `with X as Y: pass` |
+| `assert` | Assert (ensure) that somehting is true. | `assert False`, `"Error!"`|
+| `break` | Stop this loop right now. | `while True: break` |
+| `class` | Define a class. | `Class Person (object)` |
+| `continue` | Don't process more of the loop, do it again. | `while True: continue` |
+| `def` | Define a function. | `def X(): pass` |
+| `del` | Delete from dictionary. | `del X[Y]` |
+| `elif` | Else if condition. | `if: X; elif: Y;; else: J` |
+| `else` | Else condition. | `if: X; elfi: Y; else: J` |
+| `except` | If an exception happens, do this. | `except ValueError, e: print(e)` |
+| `exec` | Run a string as Python. | `exec 'print("hello")'` |
+| `finally` | Exceptions or not, finally do this no matter what. | `finally: pass` |
+| `for` | Loop over a collection of things. | `for X in Y: pass` |
+| `from` | inport specific parts of a module. | `from X import Y` |
+| `global` | Declare that you want a global variable. | `global X` |
+| `if` | If condition. | `if: X; elif: Y; else: J` |
+| `import` | Import a module into this one to use. | `import os` |
+| `in` | Part of `for-loops`. Also a test of X in Y. | `for X in Y: pass also 1 in [1] == True` |
+| `is` | Like == to test equality. | `1 is 1 == True` |
+| `lambda` | Create a short anonymbous function. | `s = lambda y: y ** y; s(3)` |
+| `not` | Logical not. | `not True == False` |
+| `or` | Logical or. | `True or False == True` |
+| `pass` | This block is empty. | `def empty(): pass` |
+| `print` | Print this string. | `print("this string")` |
+| `raise` | Rase an exception when things go wrong. | `rause ValueError("No)` |
+| `return` | Exit the function with a return value. | `def X(): return Y` |
+| `try` | Try this block, and if exception, go to `except`. | `try: pass` |
+| `while` | While loop. | `while X: pass` |
+| `with` | With an expression as a variable do. | `with X as Y: pass` |
+| `yield` | Pause here and return to caller. | `def X(): yield Y; X()
+.next() |
+
+# Data Types
+
+| Type | Description | Example |
+| - | - | - |
+| `True` | | True Boolean value. | `True or False == True` |
+| `False` | False boolean value. | `False and True == False` |
+| `None` | Represents "nothing or "no value." | `x = None` |
+| `bytes` | Stores bytes, maybe of text, PNG, file, etc. | `x = b"hello"` |
+| `strings` | Stores textual information. | `x = "hello"` |
+| `numbers` | Stores integers. | `i = 100` |
+| `floats` | Stores decimals. | `i = 10.389` |
+| `lists` | Stores a list of things. | `j = [1,2,3,4]` |
+| `dicts` | Stores a key=value mapping of things. | `e = {'x': 1, 'y': 2}` |
+
+# String Escape Sequences
+
+| Escape | Description |
+| - | - |
+| `\\` | Backslash |
+| `\'` | Single-quote |
+| `\"` | Double-quote | 
+| `\a` | Bell |
+| `\b` | Backspace |
+| `\f` | Formfeed |
+| `\n` | Newline |
+| `\r` | Carriage |
+| `\t` | Tab |
+| `\v` | Vertical Tab |
+
+# Operators
+
+| Operator | Description | Example |
+| - | - | - |
+| `+` | Addition | ` 2 + 4 == 6 ` |
+| `-` | Subtraction | `2 - 4 == -2` |
+| `*` | Multiplication | `2 * 4 == 8` |
+| `**` | Power of | `2 ** 4 == 16` |
+| `/` | Division | `2 / 4 == 0.5`|
+| `//` | Floor division | `2 // 4 == 0` |
+| `%` | String interpolate or modulus | `2 % 4 == 2` |
+| `<` | Less than | ` 4 < 4 == False` |
+| `>` | Greater than | `4 > 4 == False` |
+| `<=`| Less than equal | `4 <= 4 == True` |
+| `>=` | Greater than equal | `4 >= 4 == True` |
+| `==` | Equal | `4 == 5 == False` |
+| `!=` | Not equal | `4 != 5 == True` |
+| `( )`
+
