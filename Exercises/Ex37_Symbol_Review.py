d412e2c66a47928a1be3a5299ee3714ad79871ec
axecopfire <braun.sn11@gmail.com>
2017-12-18 10:27:46 -0500

    tummy med added, inventory init, order init

--- Exercises/Ex37_Symbol_Review.py
+++ Exercises/Ex37_Symbol_Review.py
@@ -1,10 +1,14 @@
-def Pharmacy():
-    cough_med = 3
-    head_med = 2
+def shipment():
 
-    assert cough_med or head_med !<=0:
-    print('medicine too low. Please recheck math.')
+inventory = { cough_med:cough_med_amt, head_med:head_med_amt, tummy_med:tummy_med_amt }
+
+def Pharmacy():
+
+    #assert cough_med or head_med >= 0
+    #print('medicine too low. Please recheck math.')
 
     if cough_med and head_med >= 1:
         # Requires an as statement
-        print(f'The amount of cough medicine left in your building: {cough_med} \n The amount of head medicine left in your building: {head_med}')
+        print(f'Cough medicine: {cough_med_amt} \n Head medicine: {head_med_amt} \n Tummy medicine: {tummy_med_amt} ')
+
+Pharmacy()
