4b974c3a827511c11bf740aae05c25e058027051
axecopfire <braun.sn11@gmail.com>
2017-12-18 10:46:59 -0500

    invReq init

--- Exercises/Ex37_Symbol_Review.py
+++ Exercises/Ex37_Symbol_Review.py
@@ -1,14 +1,22 @@
-def shipment():
+cough_med_amt = 10
+head_med_amt = 10
+tummy_med_amt = 10
 
-inventory = { cough_med:cough_med_amt, head_med:head_med_amt, tummy_med:tummy_med_amt }
+inventory = { 'cough_med':cough_med_amt, 'head_med':head_med_amt, 'tummy_med':tummy_med_amt }
 
-def Pharmacy():
+def Pharmacy(inventory):
 
     #assert cough_med or head_med >= 0
     #print('medicine too low. Please recheck math.')
 
-    if cough_med and head_med >= 1:
+    if cough_med_amt and head_med_amt >= 1:
         # Requires an as statement
-        print(f'Cough medicine: {cough_med_amt} \n Head medicine: {head_med_amt} \n Tummy medicine: {tummy_med_amt} ')
+        print(f'Cough medicine: {cough_med_amt} \n Head medicine: {head_med_amt} \n Tummy medicine: {tummy_med_amt}')
 
-Pharmacy()
+def InvReq(inventory):
+    req =[]
+    for i in range(inventory):
+        if inventory[i] <= 5:
+            req.append(10 - inventory[i])
+
+Pharmacy(inventory)
